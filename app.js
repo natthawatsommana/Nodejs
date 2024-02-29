03175cd826eabf4ff3cdae263ea56d3a3ea28601
Natthawat Sommana <sommana_n@outlook.com>
2024-03-01 00:01:08 +0700

    Use templates

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const morgan = require('morgan');
+//const debug = require('debug')('app');
+//const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port =  3000;
 
-app.get("/", (req,res) => {
+//app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.get("/",(req,res) => {
 
     res.send('Hello night');
-
 })
 
-app.listen(port, () =>{
-    debug("Listening on port %d", port);
+app.listen(port, ()=>{
+
+    console.log("Listening on port %d",port);
 
 })
 
